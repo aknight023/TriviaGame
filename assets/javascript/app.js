d5cffe5c610cadee025afac9914908e53ecc756d
anthonyknight023@gmail.com <anthonyknight023@gmail.com>
2018-01-13 14:11:17 -0500

    question checker

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,35 +9,35 @@ $(document).ready(function() {
 			q: "BB-8 is an astromech droid from what film?", 
 			a: ["Star Wars" ,"Star Track", "RobotCop", "Transformers"], 
 			correct: 0,
-			form: "#form1"
+			form: "#form0"
 
 		}, {
 
 			q: "Who does the voice over for Dory from Finding Nemo and Finding Dory?", 
 			a: ["Jodi Benson" ,"Ellen Degeneres", "Tom Hanks","Joe Ranft"], 
 			correct: 1,
-			form: "#form2"
+			form: "#form1"
 
 		}, {
 
 			q: "What school does Harry Potter attend?", 
 			a: ["Florida State" ,"Yale", "Boston U", "Hogwarts"], 
 			correct: 3,
-			form: "#form3"
+			form: "#form2"
 
 		}, {
 
 			q: "In which Star Wars film did the Ewoks first appear?", 
 			a: ["Revenge of the Sith" ,"Attack of the Clones", "The Empire Strikes Back","Return of the Jedi" ], 
 			correct: 3,
-			form: "#form4"
+			form: "#form3"
 
 		},{
 
 			q: "What fictional city is the home of Batman?", 
 			a: [ "Gateway City","Gotham City","The Hidden City", "Zenith City" ], 
 			correct: 1,
-			form: "#form5"
+			form: "#form4"
 
 		}];
    
@@ -82,29 +82,78 @@ $(document).ready(function() {
             var allquestions = $('<h1 class="header'+i+'">'+questionAll[i].q+'</h1>');
 			$(".question"+i).append(allquestions);
 
-			var allforms = $('<form class ="form'+i+'"></form>');
-	        $(".header"+i).append(allforms);
+			// this was append inside the h1 element
+			// var allforms = $('<form class ="form'+i+'"></form>');
+	  //       $(".header"+i).append(allforms);
+	  		var allforms = $('<form class ="form'+i+'"></form>');
+	        $(".question"+i).append(allforms);
 
 	        for (var j = 0; j < 4 ; j ++) {
-
-	        	$('.form'+i).append('<input type="radio" name="choice" value="">'+questionAll[i].a[j]+'</input>');
+	        	var inputButn = $('<input type="radio" name="choice" value="'+(questionAll[i].correct == j?1:0) +'">'+questionAll[i].a[j]+'</input><br>');
+	        	$('.form'+i).append(inputButn);
 
 	        };
 
 
-        };
-        	
-        	// var allforms = $('<form></form>');
-	        // $("h1").append(allforms);
+        };       	
 
-	        // for (var j = 0; j < 4 ; j ++) {
-
-	        // 	$('form').append('<input type="radio" name="choice" value="">'+questionAll[i].a[j]+'</input>');
-
-	        // };
 
 
 	});
+
+	$('.submit').on ('click', function () {
+		// var submitBtn = $(this);
+		// $('.timer').hide();
+		// $('.content').hide();
+		// submitBtn.hide();
+		
+
+		var radioCheck = $(":radio:checked");
+		unanswer =	questionAll.length - radioCheck.length;
+		console.log(unanswer);
+		radioCheck.each( function (elem) {
+
+			console.log(parseInt($(this).val()));
+			if(parseInt($(this).val())) {
+				correct++;
+				console.log ("Correct " + correct);
+
+			} else  {
+				wrong++;
+				console.log ("Wrong " + wrong);
+			}
+
+		});
+
+			$(":radio").not(":checked").each( function (elem) {
+
+				// if ($(this) = 4) {
+				// 	unanswer = 1;
+				// } 
+				// else if ($(this) = 8) {
+				// 	unanswer= 2; 
+				// }
+				// else if ($(this) = 12) {
+				// 	unanswer= 3 ;
+				// }
+				// else if ($(this) = 16) {
+				// 	unanswer= 4 ;
+				// }
+				// else if ($(this) = 20) {
+				// 	unanswer= 5 ;
+				// }
+
+				console.log(unanswer);
+				
+		
+
+			});
+
+
+
+	});
+
+
 
 
 
